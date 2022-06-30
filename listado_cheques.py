c8408d4311905d64ecda7a90bc8f7990ff1fd95b
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2022-06-30 09:26:32 -0300

    filtro por estado agregado

--- listado_cheques.py
+++ listado_cheques.py
@@ -3,6 +3,10 @@ import csv
 
 archivo,dni,salida,tipo = (sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
 
+if len(sys.argv) < 6:
+  estado = "sin entrada"
+else:
+  estado = sys.argv[5]
 
 def filtrarChequesPorDni(listado,dni):
   return(list(listado := filter(lambda lista:lista[-3] == dni, listado)))
@@ -10,26 +14,32 @@ def filtrarChequesPorDni(listado,dni):
 def filtrarChequesPorTipo(listado, tipo):
   return(list(listado := filter(lambda lista:lista[-2] == tipo, listado)))
 
+def filtrarChequesPorEstado(listado,estado):
+  if estado == "sin entrada":
+    return list(listado)    
+  else:
+    return(list(listado := filter(lambda lista:lista[-1] == estado, listado)))
 
 def filtrarListadoDeCheques(archivo,dni,salida,tipo,estado="sin entrada",fecha="sin entrada"):
   rutaArchivo = open(archivo,"r")
   archivoCSV = list(csv.reader(rutaArchivo))
   chequesPorDni = filtrarChequesPorDni(archivoCSV,dni)
   chequesPorTipo = filtrarChequesPorTipo(chequesPorDni, tipo)
+  chequesPorEstado = filtrarChequesPorEstado(chequesPorTipo,estado)
   if salida == "PANTALLA":
     print(archivoCSV[0])
-    for linea in chequesPorTipo:
+    for linea in chequesPorEstado:
       print(linea)
   else:
     archivoAImprimir = open("listado_de_cheques.csv","w")
     archivoAImprimirCSV = csv.writer(archivoAImprimir)
     archivoAImprimirCSV.writerow(archivoCSV[0])
-    archivoAImprimirCSV.writerows(chequesPorTipo)
+    archivoAImprimirCSV.writerows(chequesPorEstado)
     archivoAImprimir.close()
   rutaArchivo.close()
 
 
-filtrarListadoDeCheques(archivo,dni,salida,tipo)
+filtrarListadoDeCheques(archivo,dni,salida,tipo,estado)
 
 
 
